8bf518097c282b6c95b8b77b5573c25e5f9e9b91
dabbechbilel <93347337+dabbechbilel@users.noreply.github.com>
2024-09-24 11:12:54 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/Foyer_BI10/FoyerBi10Application.java
@@ -0,0 +1,14 @@
+package tn.esprit.Foyer_BI10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class FoyerBi10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoyerBi10Application.class, args);
+	}
+
+}
---
+++ src/main/java/tn/esprit/Foyer_BI10/entites/TypeChambre.java
@@ -0,0 +1,6 @@
+package tn.esprit.Foyer_BI10.entites;
+
+public enum TypeChambre {
+
+    SIMPLE , DOUBLE ,TRIPLE
+}
